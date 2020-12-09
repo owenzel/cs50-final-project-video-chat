1603885fccc0419487cce8a85382fd98c4db89e4
owenzel <oliviacwenzel@gmail.com>
2020-12-09 00:03:51 -0500

    Added comments

--- public/script.js
+++ public/script.js
@@ -1,56 +1,62 @@
-const socket = io('/')
-const videoGrid = document.getElementById('video-grid')
+// Handle connections to a video chat room (users visiting a URL with a certain ID) by accessing the client's camera and microphone
+// and adding them to the grid of videos. Also handle disconnections from the room (closing the browser tab).
+
+const socket = io('/');
+const videoGrid = document.getElementById('video-grid');
 const myPeer = new Peer(undefined, {
   host: '/',
   port: '3001'
-})
-const myVideo = document.createElement('video')
-myVideo.muted = true
-const peers = {}
+});
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+const peers = {};
 navigator.mediaDevices.getUserMedia({
   video: true,
   audio: true
 }).then(stream => {
-  addVideoStream(myVideo, stream)
+  addVideoStream(myVideo, stream);
 
   myPeer.on('call', call => {
-    call.answer(stream)
-    const video = document.createElement('video')
+    call.answer(stream);
+    const video = document.createElement('video');
     call.on('stream', userVideoStream => {
       addVideoStream(video, userVideoStream)
-    })
+    });
   })
 
   socket.on('user-connected', userId => {
-    connectToNewUser(userId, stream)
+    connectToNewUser(userId, stream);
   })
 })
 
 socket.on('user-disconnected', userId => {
-  if (peers[userId]) peers[userId].close()
+  if (peers[userId])
+  {
+    peers[userId].close();
+  }
 })
 
 myPeer.on('open', id => {
-  socket.emit('join-room', ROOM_ID, id)
+  socket.emit('join-room', ROOM_ID, id);
 })
 
 function connectToNewUser(userId, stream) {
-  const call = myPeer.call(userId, stream)
-  const video = document.createElement('video')
+  const call = myPeer.call(userId, stream);
+  const video = document.createElement('video');
   call.on('stream', userVideoStream => {
-    addVideoStream(video, userVideoStream)
+    addVideoStream(video, userVideoStream);
   })
   call.on('close', () => {
-    video.remove()
+    video.remove();
   })
 
-  peers[userId] = call
+  peers[userId] = call;
 }
 
 function addVideoStream(video, stream) {
-  video.srcObject = stream
+  video.srcObject = stream;
   video.addEventListener('loadedmetadata', () => {
-    video.play()
+    video.play();
   })
-  videoGrid.append(video)
+  videoGrid.append(video);
 }
--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 //CREDIT FOR ALL FILES: https://www.youtube.com/watch?v=DvlyzDZDEq4&t=102s
 
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
+// Set up the server for web development with Express, Socket.io, and UUID (for generating random IDs for the URLs)
+const express = require('express'); // Web development framework
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const { v4: uuidV4 } = require('uuid');
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+//Set the view engine to EJS
+app.set('view engine', 'ejs');
 
+// Middleware
+app.use(express.static('public'));
+
+// Handle GET requests to the default URL: generate a new ID for a new video chat room and redirect the user to that URL/room
 app.get('/', (req, res) => {
   res.redirect(`/${uuidV4()}`)
 })
 
+// Handle GET requests to the default URL with a slash followed by a room ID
 app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room })
 })
 
+// Handle connections to video chat rooms
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
     socket.join(roomId)
